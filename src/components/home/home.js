c2844785b017868b54c29da7101cc3a2cdd7f18a
Palash Debnath <palash@cefalo.com>
2021-03-22 18:49:16 +0600

    PALASH - uncommited changes

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -1,28 +1,27 @@
 import { useState } from 'react';
+import BlogList from '../blogs/bloglist';
 
 const Home = () => {
 
     const [blogPosts, setBlogPosts] = useState(
         [
-            {title: "Blog Post 1 Title", body: "Blog post 1 body", author: 'Palash', id: 1},
-            {title: "Blog Post 2 Title", body: "Blog post 2 body", author: 'Pritam', id: 2},
-            {title: "Blog Post 3 Title", body: "Blog post 3 body", author: 'Ghosh', id: 3}
+            { title: "Blog Post 1 Title", body: "Blog post 1 body", author: 'Palash', id: 1 },
+            { title: "Blog Post 2 Title", body: "Blog post 2 body", author: 'Pritam', id: 2 },
+            { title: "Blog Post 3 Title", body: "Blog post 3 body", author: 'Ghosh', id: 3 }
         ]
     )
-    
-    return ( 
+
+    const handleDelete = (id) => {
+        const newBlogPosts = blogPosts.filter((blog) => blog.id !== id);
+        setBlogPosts(newBlogPosts);
+    }
+
+    return (
         <div className="home">
-            {
-                blogPosts.map((blog) => (
-                    <div className="blog-preview" key={blog.id}>
-                        <h2>{ blog.title }</h2>
-                        <h4>Author: {blog.author}</h4>
-                        <p>Content: {blog.body}</p>
-                    </div>
-                ))
-            }
+            <BlogList blogPosts={blogPosts} title="All Blogposts" handleDelete={handleDelete}></BlogList>
+            {/* <BlogList blogPosts={blogPosts.filter((blog) => blog.author === 'Pritam')} title="All Blogposts"></BlogList> */}
         </div>
-     );
+    );
 }
- 
+
 export default Home;
